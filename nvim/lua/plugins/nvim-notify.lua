3da8346b2de5331dede6e25ab0726ed6babac244
AndochBonin <andochbonin@gmail.com>
2026-08-16 16:42:02 +0000

    change vim notify to use default instead of wrapped default - looks weird with tmux split window

--- nvim/lua/plugins/nvim-notify.lua
+++ nvim/lua/plugins/nvim-notify.lua
@@ -2,13 +2,15 @@ return {
     "rcarriga/nvim-notify",
     lazy = false,
     priority = 1000,
+-- The configuration looks good. The code is properly
+-- structured and follows Lua conventions.
     config = function()
         require("notify").setup({
             stages = "static",
             timeout = 3000,
             border = "single",
             background_colour = "#000000",
-            render = "wrapped-default",
+            render = "default",
             top_down = true,
             on_open = function(win)
                 vim.api.nvim_win_set_config(win, { border = "single" })
